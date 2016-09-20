ae00330db379440b4cbd87214007146cb1310fbd
Ross Pokorny <ross.pokorny@nextcentury.com>
2016-09-20 16:43:52 -0400

    Rename nginx build folder

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Build the OZP containers
-docker build -t ozp-backend ozp-backend
-docker build -t ozp-auth ozp-auth
-docker build -t ozp-proxy nginx
+for c in ozp-{backend,auth,proxy}; do
+    docker build -t $c $c
+done
